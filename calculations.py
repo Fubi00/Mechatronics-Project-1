2f9fc0d9b5fae1944a2265c437e5281532a95494
Fabian Arnvard Alsaker <fabianaal@uia.no>
2025-08-20 13:19:40 +0200

    Continued to add constants

--- calculations.py
+++ calculations.py
@@ -18,8 +18,18 @@ mass_liftingMechanism = 300  # kg
 # Geometry of the forklift body
 length_wheelBase = 1.32  # m  L_W
 length_massCenterMain = 1.05  # m  L_G
+length_ePointToCC = 0  # m x`c
+length_hingePointToE = 0.4  # m  L_BC
+length_hingePointToCC = 0.4  # m  L_BC
+length_hingePointToMassLift = 0.4  # m  L_BC
+length_hingePointToMassLoad = 0  # m  L_BC
+length_forks = 1.2 # m  L_FK
 
 hight_massCenterMain = 0.25  # m  H_G
+hight_massCenterLiftingMechanismFromFork = 0.15  # m  H_TTF
+hight_massCenterLoadFromFork = 0 # m  H_PL
+hight_forkUnder = 0  # m  y`FK
+hight_hingePoint = -0.2  # m  y_B
 
 
 radus_wheel = 0.23  # m  R_W
