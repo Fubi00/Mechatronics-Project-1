d2b5db31982faa105ac2a3c17e8bad3aeb3596af
Fabian Arnvard Alsaker <fabianaal@uia.no>
2025-08-20 11:52:02 +0200

    Started to add constents

--- calculations.py
+++ calculations.py
@@ -4,3 +4,24 @@ This sctipt contains functions and data to perform various calculations in our w
 The project is part of the course MAS238 "Mekkatronikkprosjekt 1" at the University of Agder (UiA).
 """
 ""Created by: [Fabian A. Alsaker, Julian F. Idsø, Daniel, Nasar, 2025-08-20]""
+
+import math
+
+
+# Constants for gyometry
+
+# Masses of the components
+mass_loade = 1500  # kg
+mass_mainBody = 3500  # kg
+mass_liftingMechanism = 300  # kg
+
+# Geometry of the forklift body
+length_wheelBase = 1.32  # m  L_W
+length_massCenterMain = 1.05  # m  L_G
+
+hight_massCenterMain = 0.25  # m  H_G
+
+
+radus_wheel = 0.23  # m  R_W
+
+# Geometry of the lifting mechanism
