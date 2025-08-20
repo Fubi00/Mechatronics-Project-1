99e3801c7d35f6c0f76e0723710a76e96ca27aea
Fabian Arnvard Alsaker <fabianaal@uia.no>
2025-08-20 11:42:25 +0200

    Added creators

--- calculations.py
+++ calculations.py
@@ -3,3 +3,4 @@
 This sctipt contains functions and data to perform various calculations in our worck in designing the lifting mechanism for an forcklift.
 The project is part of the course MAS238 "Mekkatronikkprosjekt 1" at the University of Agder (UiA).
 """
+""Created by: [Fabian A. Alsaker, Julian F. Idsø, Daniel, Nasar, 2025-08-20]""
